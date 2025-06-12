675939226dee1cd8e674a3636e73e61c33c4dedc
Ilir Tzaferai <ilt0055@my.londonmet.ac.uk>
2025-06-12 05:11:05 +0100

    c

--- billing/views.py
+++ billing/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
-from .models import Invoice, Customer
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .models import Invoice, Customer
 
+# Custom LoginView to redirect if already logged in
+class CustomLoginView(LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
+
+# Rest of your views:
+def home(request):
+    return render(request, 'billing/home.html')
 
 @login_required
 def invoice_list(request):
@@ -15,16 +26,13 @@ def customer_list(request):
     customers = Customer.objects.all()
     return render(request, 'billing/customer_list.html', {'customers': customers})
 
-def home(request):
-    return render(request, 'billing/home.html')
-
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # auto-login after register
-            return redirect('home')  # or 'invoice_list' if you prefer
+            login(request, user)
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'billing/register.html', {'form': form})
